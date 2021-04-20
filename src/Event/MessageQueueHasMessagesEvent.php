176ecbb453e2f4f8c575e5836983fff519af74ec
tomme87 <tom.roar@halloverden.no>
2021-04-20 18:16:32 +0200

    ADD: MessageQueueHasMessagesEvent

---
+++ src/Event/MessageQueueHasMessagesEvent.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace HalloVerden\MessengerQueueNotificationsBundle\Event;
+
+
+/**
+ * Class MessageQueueHasMessagesEvent
+ *
+ * @package HalloVerden\MessengerQueueNotificationsBundle\Event
+ */
+class MessageQueueHasMessagesEvent extends AbstractMessageQueueEvent {
+
+}
