c8c26c920245cbeae5644b35ae02b295d12f0673
tomme87 <tom.roar@halloverden.no>
2021-04-20 18:15:43 +0200

    ADD: HalloVerdenMessengerQueueNotificationsBundle

---
+++ src/HalloVerdenMessengerQueueNotificationsBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace HalloVerden\MessengerQueueNotificationsBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class HalloVerdenMessengerQueueNotificationsBundle
+ *
+ * @package HalloVerden\MessengerQueueNotificationsBundle
+ */
+class HalloVerdenMessengerQueueNotificationsBundle extends Bundle {
+
+}
