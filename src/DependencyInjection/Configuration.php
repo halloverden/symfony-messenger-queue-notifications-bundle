afa36fc5bb590e04bac46c19ff2e8b630eb0e4f3
tomme87 <tom.roar@halloverden.no>
2021-04-20 18:15:54 +0200

    ADD: Configuration

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace HalloVerden\MessengerQueueNotificationsBundle\DependencyInjection;
+
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration
+ *
+ * @package HalloVerden\MessengerQueueNotificationsBundle\DependencyInjection
+ */
+class Configuration implements ConfigurationInterface {
+
+  /**
+   * @inheritDoc
+   */
+  public function getConfigTreeBuilder(): TreeBuilder {
+    $treeBuilder = new TreeBuilder('hallo_verden_messenger_queue_notifications');
+
+    $treeBuilder->getRootNode()
+      ->children()
+        ->arrayNode('message_queue_stopped_event')
+          ->addDefaultsIfNotSet()
+          ->children()
+            ->arrayNode('transports')
+              ->arrayPrototype()
+                ->children()
+                  ->integerNode('max_handle_time')->defaultValue(300)->end()
+                  ->integerNode('event_interval')->defaultValue(1800)->end()
+                  ->integerNode('max_events')->defaultValue(100)->end()
+                ->end()
+              ->end()
+            ->end()
+          ->end()
+        ->end()
+      ->end();
+
+    $treeBuilder->getRootNode()
+      ->children()
+        ->arrayNode('message_queue_has_messages_event')
+          ->addDefaultsIfNotSet()
+          ->children()
+            ->arrayNode('transports')
+              ->arrayPrototype()
+                ->children()
+                  ->integerNode('max_messages')->defaultValue(1)->end()
+                  ->integerNode('event_interval')->defaultValue(1800)->end()
+                  ->integerNode('max_events')->defaultValue(100)->end()
+                ->end()
+              ->end()
+            ->end()
+          ->end()
+        ->end()
+      ->end();
+
+    return $treeBuilder;
+  }
+
+}
